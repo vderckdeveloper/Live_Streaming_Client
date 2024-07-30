1d67cd90c919b624e02b440cf4bf5f5b51f8d5c7
vderckdeveloper <vderckdeveloper@gmail.com>
2024-07-30 11:30:53 +0900

    feat: toolbar

--- src/component/toolbar/toolbar.tsx
+++ src/component/toolbar/toolbar.tsx
@@ -1,12 +1,50 @@
+"use client";
+import { useState, useEffect } from "react";
 import Image from "next/image"
 import EduMeetLogoImage from '../../../public/image/toolbar/240730_eduMeet_logo_Ver1.0.png';
 
+// style
+import styles from '@/styles/toolbar/toolbar.module.css';
+
+interface DateFormatOptions {
+    month: 'long' | 'short' | 'narrow';
+    day: 'numeric' | '2-digit';
+    weekday: 'long' | 'short' | 'narrow';
+    hour: 'numeric' | '2-digit';
+    minute: 'numeric' | '2-digit';
+    hour12: boolean;
+}
+
 function Toolbar() {
 
+     const [currentTime, setCurrentTime] = useState<string>("");
+
+    useEffect(() => {
+        const updateCurrentTime = () => {
+            const now: Date = new Date();
+            const options: DateFormatOptions = { 
+                month: 'long', day: 'numeric', weekday: 'long', 
+                hour: 'numeric', minute: 'numeric', hour12: true 
+            };
+            const formattedTime = now.toLocaleDateString('ko-KR', options);
+            setCurrentTime(formattedTime);
+        };
+
+        updateCurrentTime();
+        const intervalId = setInterval(updateCurrentTime, 1000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
     return (
-        <section>
-            <nav>
-                <Image src={EduMeetLogoImage} width={567} height={189} alt='로고 이미지' />
+        <section className={styles['container']}>
+            <nav className={styles['wrapper']}>
+                <div className={styles['imageBox']}>
+                    <Image src={EduMeetLogoImage} width={567} height={189} alt='로고 이미지' />
+                </div>
+                <div className={styles['timeBox']}>
+                    <p>{currentTime}</p>
+                </div>
             </nav>
         </section>
     )
