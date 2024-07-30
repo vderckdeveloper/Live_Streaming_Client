8f245840a03c67b41c7c41af81ef35df608870ef
vderckdeveloper <vderckdeveloper@gmail.com>
2024-07-30 16:43:45 +0900

    feat: main slide

--- src/component/main/mainSlide.tsx
+++ src/component/main/mainSlide.tsx
@@ -3,8 +3,9 @@ import { useState } from "react";
 
 // image
 import groupVideoImage from '../../../public/image/main/240730_mainSlide_Image(1)_Ver1.0.png';
+import recordVideoImage from '../../../public/image/main/240730_mainSlide_Image(2)_Ver1.0.png';
 
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 // Import Swiper React components
 import { Swiper, SwiperSlide, SwiperClass } from "swiper/react";
 
@@ -19,17 +20,46 @@ import { Autoplay, Navigation, Pagination } from "swiper/modules";
 
 import styles from '@/styles/main/mainSlide.module.css';
 
-function SlideContent() {
+interface SlideListProps {
+    imagePath: StaticImageData;
+}
+
+function SlideContent(props: SlideListProps) {
+
+    const imagePath = props.imagePath;
+
     return (
         <section>
-            <div className={styles['imageBox']}>
-                <Image src={groupVideoImage} width={500} height={500} alt="그룹 비디오 이미지" quality={100} />
+            <div className={styles['image-box']}>
+                <Image src={imagePath} width={500} height={500} alt="그룹 비디오 이미지" quality={100} />
             </div>
         </section>
     );
 }
 
 function MainSlide() {
+
+    // slide list
+    const slideList: SlideListProps[] = [
+        {
+            imagePath: groupVideoImage,
+        },
+        {
+            imagePath: recordVideoImage,
+        },
+        {
+            imagePath: groupVideoImage,
+        },
+        {
+            imagePath: recordVideoImage,
+        },
+        {
+            imagePath: groupVideoImage,
+        },
+        {
+            imagePath: recordVideoImage,
+        },
+    ];
 
     const [swiper, setSwiper] = useState<SwiperClass>();
 
@@ -44,46 +74,64 @@ function MainSlide() {
     return (
         <main className={styles['container']}>
             <div className={styles['wrapper']}>
-                {/* swiper */}
-                <Swiper
-                    slidesPerView={5}
-                    spaceBetween={30}
-                    autoplay={{
-                        delay: 2000,
-                        disableOnInteraction: false,
-                    }}
-                    loop={true}
-                    breakpoints={{
-                        0: {
-                            slidesPerView: 1,
-                        },
-                    }}
-                    onSwiper={(e) => { setSwiper(e) }}
-                    modules={[Autoplay, Navigation, Pagination]}
-                    pagination={true}
-                    className={styles['mainSlide']}
-                >
-                    <SwiperSlide>
-                        <SlideContent />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <SlideContent />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <SlideContent />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <SlideContent />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <SlideContent />
-                    </SwiperSlide>
-                </Swiper>
-                {/* navigation button */}
-                <article>
-                    <button onClick={handlePrev}>이전</button>
-                    <button onClick={handleNext}>다음</button>
-                </article>
+                <div className={styles['slide-frame']}>
+                    {/* button box */}
+                    <article className={`${styles['button-box']} ${styles['left-button-box']}`}>
+                        <button onClick={handlePrev}>
+                            <svg width="24" height="24" viewBox="0 0 24 24">
+                                <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12l4.58-4.59z"></path>
+                            </svg>
+                        </button>
+                    </article>
+                    {/* swiper */}
+                    <Swiper
+                        slidesPerView={5}
+                        spaceBetween={30}
+                        autoplay={{
+                            delay: 2000,
+                            disableOnInteraction: false,
+                        }}
+                        loop={true}
+                        breakpoints={{
+                            0: {
+                                slidesPerView: 1,
+                            },
+                        }}
+                        onSwiper={(e) => { setSwiper(e) }}
+                        modules={[Autoplay, Navigation, Pagination]}
+                        pagination={{ clickable: true }}
+                        className={`${styles['main-slide']} main-slide`}
+                    >
+                        {
+                            slideList.map((item, index) => {
+                                const imagePath = item.imagePath;
+
+                                return (
+                                    <SwiperSlide key={index} >
+                                        <SlideContent
+                                            imagePath={imagePath}
+                                        />
+                                    </SwiperSlide>
+                                );
+                            })
+                        }
+                    </Swiper>
+                    {/* navigation button */}
+                    <article className={`${styles['button-box']} ${styles['right-button-box']}`}>
+                        <button onClick={handleNext}>
+                            <svg width="24" height="24" viewBox="0 0 24 24">
+                                <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6-6-6z"></path>
+                            </svg>
+                        </button>
+                    </article>
+                </div>
+                <div className={styles['text-frame']}>
+                    <div className={styles['text-box']}>
+                        <h2>다자간 화상 통화 교육을 위한 최고의 선택!</h2>
+                        <p>Edu Meet에서 많은 수의 인원에게 실시간 스트리밍을 시작하세요!</p>
+                        <button>시작하기</button>
+                    </div>
+                </div>
             </div>
         </main>
     );
