ba3ce1211f65cbe69c40f3df17573376b93826cc
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-07 22:26:48 +0900

    feat: multi screen

--- src/component/stream/stream.tsx
+++ src/component/stream/stream.tsx
@@ -1,6 +1,10 @@
+"use client";
+import { useState } from 'react';
 import styles from '@/styles/stream/stream.module.css';
 
 function Stream() {
+
+    const [ screenNum, setScreenNum ] = useState<number>(1);
 
     return (
         <section className={styles['container']}>
@@ -11,6 +15,27 @@ function Stream() {
                         <video autoPlay playsInline muted></video>
                     </div>
                     <h2 className={styles['video-text']}>내 화면</h2>
+                </div>
+                {/* other screen */}
+                <div className={styles['screen']}>
+                    <div className={styles['video-box']} style={{backgroundColor: 'black'}}>
+                        <video autoPlay playsInline muted></video>
+                    </div>
+                    <h2 className={styles['video-text']}>참여자1 화면</h2>
+                </div>
+                {/* other screen */}
+                <div className={styles['screen']}>
+                    <div className={styles['video-box']} style={{backgroundColor: 'black'}}>
+                        <video autoPlay playsInline muted></video>
+                    </div>
+                    <h2 className={styles['video-text']}>참여자2 화면</h2>
+                </div>
+                {/* other screen */}
+                <div className={styles['screen']}>
+                    <div className={styles['video-box']} style={{backgroundColor: 'black'}}>
+                        <video autoPlay playsInline muted></video>
+                    </div>
+                    <h2 className={styles['video-text']}>참여자3 화면</h2>
                 </div>
             </div>
         </section>
