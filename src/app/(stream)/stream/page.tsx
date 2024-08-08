43e7b688f3f5ed302f4821f14e7e0667f5ee80ce
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-08 17:47:29 +0900

    feat: change file strcuture

--- src/app/(stream)/stream/page.tsx
+++ src/app/(stream)/stream/page.tsx
@@ -1,12 +1,10 @@
 // components
 import Stream from "@/component/stream/stream";
-import Setting from "@/component/stream/setting";
 
 export default function StreamPage() {
   return (
     <>
       <Stream />
-      <Setting />
     </>
   );
 }
--- src/component/stream/stream.tsx
+++ src/component/stream/stream.tsx
@@ -1,44 +1,14 @@
 "use client";
-import { useState } from 'react';
-import styles from '@/styles/stream/stream.module.css';
+import Screen from "./screen";
+import Setting from "./setting";
 
 function Stream() {
 
-    const [ screenNum, setScreenNum ] = useState<number>(1);
-
     return (
-        <section className={styles['container']}>
-            <div className={styles['wrapper']}>
-                {/* my screen */}
-                <div className={styles['screen']}>
-                    <div className={styles['video-box']} style={{backgroundColor: 'black'}}>
-                        <video autoPlay playsInline muted></video>
-                    </div>
-                    <h2 className={styles['video-text']}>내 화면</h2>
-                </div>
-                {/* other screen */}
-                <div className={styles['screen']}>
-                    <div className={styles['video-box']} style={{backgroundColor: 'black'}}>
-                        <video autoPlay playsInline muted></video>
-                    </div>
-                    <h2 className={styles['video-text']}>참여자1 화면</h2>
-                </div>
-                {/* other screen */}
-                <div className={styles['screen']}>
-                    <div className={styles['video-box']} style={{backgroundColor: 'black'}}>
-                        <video autoPlay playsInline muted></video>
-                    </div>
-                    <h2 className={styles['video-text']}>참여자2 화면</h2>
-                </div>
-                {/* other screen */}
-                <div className={styles['screen']}>
-                    <div className={styles['video-box']} style={{backgroundColor: 'black'}}>
-                        <video autoPlay playsInline muted></video>
-                    </div>
-                    <h2 className={styles['video-text']}>참여자3 화면</h2>
-                </div>
-            </div>
-        </section>
+        <>
+            <Screen />
+            <Setting />
+        </>
     )
 }
 
