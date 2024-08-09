6a1357fac84fe6e5b7c1747ce02787d4380f59c3
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-09 13:15:32 +0900

    refractor: child component props strucuture

--- src/component/stream/setting.tsx
+++ src/component/stream/setting.tsx
@@ -10,13 +10,14 @@ interface Refs {
 
 interface ScreenProps {
     isCurrentScreenOff: boolean;
+    isScreenRecordingOff: boolean;
     onShareMyCurrentScreen: () => Promise<void>;
     onStartRecordingScreen: () => Promise<void>;
     onStopRecordingScreen: () => void;
     refs: Refs;
 }
 
-function Setting({ isCurrentScreenOff, onShareMyCurrentScreen, onStartRecordingScreen, onStopRecordingScreen, refs }: ScreenProps) {
+function Setting({ isCurrentScreenOff, isScreenRecordingOff, onShareMyCurrentScreen, onStartRecordingScreen, onStopRecordingScreen, refs }: ScreenProps) {
 
     const { streamRef } = refs;
 
@@ -26,7 +27,6 @@ function Setting({ isCurrentScreenOff, onShareMyCurrentScreen, onStartRecordingS
     // option on/off
     const [isMicOn, setIsMicOn] = useState<boolean>(true);
     const [isVideoOn, setIsVideoOn] = useState<boolean>(true);
-    const [isScreenRecordingOff, setIsScreenRecordingOff] = useState<boolean>(true);
 
     // browser width - 1024px 
     const oneThousandTwentyFourWidth = 1024;
@@ -55,11 +55,6 @@ function Setting({ isCurrentScreenOff, onShareMyCurrentScreen, onStartRecordingS
             setIsVideoOn(prevState => !prevState);
         }
     };
-
-    // record screen 
-    const onToggleRecording = () => {
-        setIsScreenRecordingOff(prevState => !prevState);
-    }
 
     // disconnect streaming and go back to main page
     const onDisconnect = () => {
@@ -143,19 +138,13 @@ function Setting({ isCurrentScreenOff, onShareMyCurrentScreen, onStartRecordingS
                         {
                             isScreenRecordingOff
                                 ?
-                                <div className={styles['svg-box']} onClick={() => {
-                                    onStartRecordingScreen();
-                                    onToggleRecording();
-                                }}>
+                                <div className={styles['svg-box']} onClick={onStartRecordingScreen}>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" width="24" height="24" viewBox="0 0 1920 1920">
                                         <path d="M960 0c529.36 0 960 430.645 960 960 0 529.36-430.64 960-960 960-529.355 0-960-430.64-960-960C0 430.645 430.645 0 960 0Zm0 112.941c-467.125 0-847.059 379.934-847.059 847.059 0 467.12 379.934 847.06 847.059 847.06 467.12 0 847.06-379.94 847.06-847.06 0-467.125-379.94-847.059-847.06-847.059Zm0 313.726c294.55 0 533.33 238.781 533.33 533.333 0 294.55-238.78 533.33-533.33 533.33-294.552 0-533.333-238.78-533.333-533.33 0-294.552 238.781-533.333 533.333-533.333Z" fillRule="evenodd" />
                                     </svg>
                                 </div>
                                 :
-                                <div className={styles['screen-record-svg-box']} onClick={() => {
-                                    onStopRecordingScreen();
-                                    onToggleRecording();
-                                }}>
+                                <div className={styles['screen-record-svg-box']} onClick={onStopRecordingScreen}>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" width="24" height="24" viewBox="0 0 1920 1920">
                                         <path d="M960 0c529.36 0 960 430.645 960 960 0 529.36-430.64 960-960 960-529.355 0-960-430.64-960-960C0 430.645 430.645 0 960 0Zm0 112.941c-467.125 0-847.059 379.934-847.059 847.059 0 467.12 379.934 847.06 847.059 847.06 467.12 0 847.06-379.94 847.06-847.06 0-467.125-379.94-847.059-847.06-847.059Zm0 313.726c294.55 0 533.33 238.781 533.33 533.333 0 294.55-238.78 533.33-533.33 533.33-294.552 0-533.333-238.78-533.333-533.33 0-294.552 238.781-533.333 533.333-533.333Z" fillRule="evenodd" />
                                     </svg>
