bb350f7a173e75544afe33e84bae3e541a8ae136
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-09 20:16:14 +0900

    feat: generate random string

---
+++ utils/utility.ts
@@ -0,0 +1,24 @@
+/***
+ * generate random string
+ * @returns returns random string combination of 'xxx-xxxx-xxx', 3digits - 4digits - 3digits 
+ */
+const generateRandomString = () => {
+    const getRandomLetter = () => {
+        const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+        return alphabet[Math.floor(Math.random() * alphabet.length)];
+    };
+
+    const generateSection = (length: number) => {
+        let result = '';
+        for (let i = 0; i < length; i++) {
+            result += getRandomLetter();
+        }
+        return result;
+    };
+
+    return `${generateSection(3)}-${generateSection(4)}-${generateSection(3)}`;
+}
+
+export {
+    generateRandomString,
+}
