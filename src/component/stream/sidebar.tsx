44e82173badbb1135c28b763047c27240f38d001
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-26 23:13:54 +0900

    feat: websocket chat connection

--- src/component/stream/sidebar.tsx
+++ src/component/stream/sidebar.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect, forwardRef, RefObject } from 'react';
 import Image from 'next/image';
+import { io, Socket } from 'socket.io-client';
 import StudySupporterImage from '../../../public/image/test/240822_testBot_Ver2.0.png';
 
 import styles from '@/styles/stream/sidebar.module.css';
@@ -9,6 +10,35 @@ interface Message {
     role: 'user' | 'assistant';
     content: string;
     timestamp: string;
+}
+
+// format server time
+const formatServerTimeDate = (dateFromServer: string) => {
+    // Parse the date string
+    const date = new Date(dateFromServer.replace(' ', 'T'));
+
+    // Extract the date part and format it manually
+    const month = date.getMonth() + 1; // Get month (zero-indexed)
+    const day = date.getDate(); // Get day
+    const formattedDate = `${month}월 ${day}일`;
+
+    // Extract the time part and format it manually
+    let hours = date.getHours();
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+    const period = hours >= 12 ? '오후' : '오전';
+
+    if (hours > 12) {
+        hours -= 12;
+    } else if (hours === 0) {
+        hours = 12;
+    }
+
+    const formattedTime = `${period} ${hours}:${minutes}:${seconds}`;
+
+    // Combine the date and time parts
+    const formattedDateTime = `${formattedDate} ${formattedTime}`;
+    return formattedDateTime;
 }
 
 // eslint-disable-next-line react/display-name
@@ -25,6 +55,9 @@ const Sidebar = forwardRef((_: any, ref: any) => {
 
     // ai loading dot
     const [aILoadingDot, setAILoadingDot] = useState<boolean>(false);
+
+    // web socket ref
+    const webSocketRef = useRef<Socket | null>(null);
 
     // scroll to bottom ref
     const scrollToBottomRef = useRef<HTMLDivElement>(null);
@@ -63,6 +96,97 @@ const Sidebar = forwardRef((_: any, ref: any) => {
     // set initial time stamp
     useEffect(() => {
         setInitialTimeStamp(new Date().toLocaleTimeString());
+    }, []);
+
+    // connect web socket
+    useEffect(() => {
+        // Connect to the WebSocket server
+        webSocketRef.current = io(`${process.env.NEXT_PUBLIC_DIRECT_CHAT_URL}/websocket/chat`, {
+            withCredentials: true,
+            autoConnect: true,
+            reconnectionAttempts: 10,
+            reconnectionDelay: 3000,
+            transports: ["websocket"],
+        });
+
+        // Event listener for successful connection
+        webSocketRef.current.on('connect', () => {
+            console.log('WebSocket Chat Connected!');
+
+            const role = {
+                role: 'user',
+            }
+    
+            webSocketRef.current?.emit('register', role);
+        });
+
+        // Event listener for receiving messages
+        webSocketRef.current.on('newFile', (message) => {
+            const role = message.role;
+            const type = message.type;
+            const content = message.message;
+            const downloadLink = message.downloadLink;
+            const timeStamp = message.register_date;
+
+            const formattedTimeStamp = formatServerTimeDate(timeStamp);
+
+            setMessages(prevMessages => [...prevMessages, {
+                role: role,
+                type: type,
+                content: content,
+                downloadLink: downloadLink,
+                timestamp: formattedTimeStamp,
+            }]);
+        });
+
+        // Event listener for receiving messages
+        webSocketRef.current.on('newMessage', (message) => {
+            const role = message.role;
+            const type = message.type;
+            const content = message.message;
+            const timeStamp = message.register_date;
+
+            const formattedTimeStamp = formatServerTimeDate(timeStamp);
+
+            setMessages(prevMessages => [...prevMessages, {
+                role: role,
+                type: type,
+                content: content,
+                timestamp: formattedTimeStamp,
+            }]);
+        });
+
+        // Event listener for isMessageWriting 
+        webSocketRef.current.on('isMessageWriting', (messageWritingStatus) => {
+            if (messageWritingStatus) {
+
+            }
+        });
+
+        webSocketRef.current.on('auth_error', (error) => {
+            console.error('Auth Error:', error);
+            alert('유저 로그인 정보가 존재하지 않습니다. 로그인후 다시 시도해주세요.');
+        });
+
+        webSocketRef.current.on('register_error', (error) => {
+            console.error('Register Error:', error);
+            alert('채팅 등록 정보에 에러가 발생했습니다. 다시 시도해주세요.');
+        });
+
+        webSocketRef.current.on('room_error', (error) => {
+            console.error('Register Error:', error);
+            alert('채팅방 입장 관련 에러가 발생했습니다. 다시 시도해주세요.');
+        });
+
+        webSocketRef.current.on('connect_error', (error) => {
+            console.error('Connection Error:', error);
+            alert('채팅 연결에 실패했습니다. 커넥션을 확인후 다시 시도해주세요.');
+        });
+
+        // Clean up on component unmount
+        return () => {
+            webSocketRef.current?.disconnect();
+        };
     }, []);
 
     return (
