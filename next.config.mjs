007c2ce212eb3ed3577bc1573594c66f140311b5
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-27 21:21:18 +0900

    feat: react strict mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // turn on restrict mode during developement phase - true or false
-    reactStrictMode: false,
+    reactStrictMode: true,
 };
 
 export default nextConfig;
