77b5e97a4b49fac3542d7e5d3ce851dcbc314b99
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-29 10:42:13 +0900

    feat: assigned id

--- src/component/stream/screen.tsx
+++ src/component/stream/screen.tsx
@@ -6,19 +6,20 @@ interface Refs {
     videoRef: React.RefObject<HTMLVideoElement>;
     streamRef: React.MutableRefObject<MediaStream | null>;
     // peer side
-    firstPeerVideoRef: React.RefObject<HTMLVideoElement>;
-    secondPeerVideoRef: React.RefObject<HTMLVideoElement>;
-    thirdPeerVideoRef: React.RefObject<HTMLVideoElement>;
+    firstPeerVideoRef: React.RefObject<HTMLVideoElement> | any;
+    secondPeerVideoRef: React.RefObject<HTMLVideoElement> | any;
+    thirdPeerVideoRef: React.RefObject<HTMLVideoElement> | any;
 }
 
 interface ScreenProps {
     isMyWebcamLoading: boolean;
     isOnlyMyVideoAvailable: boolean;
     setIsOnlyMyVideoAvailable: React.Dispatch<React.SetStateAction<boolean>>;
+    myAssignedId: string;
     refs: Refs;
 }
 
-const Screen = ({ isMyWebcamLoading, isOnlyMyVideoAvailable, setIsOnlyMyVideoAvailable, refs }: ScreenProps) => {
+const Screen = ({ isMyWebcamLoading, isOnlyMyVideoAvailable, setIsOnlyMyVideoAvailable, myAssignedId, refs }: ScreenProps) => {
 
     const { videoRef, firstPeerVideoRef, secondPeerVideoRef, thirdPeerVideoRef } = refs;
 
@@ -115,28 +116,28 @@ const Screen = ({ isMyWebcamLoading, isOnlyMyVideoAvailable, setIsOnlyMyVideoAva
                     <div className={styles['video-box']}>
                         <video autoPlay playsInline ref={videoRef}></video>
                     </div>
-                    <h2 className={styles['video-text']}>내 화면</h2>
+                    <h2 className={styles['video-text']}>{myAssignedId}</h2>
                 </div>
                 {/* other screen */}
                 <div className={styles['screen']} style={!isOnlyMyVideoAvailable && isFirstPeerVideoReady ? { display: 'flex' } : { display: 'none' }}>
                     <div className={styles['video-box']}>
                         <video autoPlay playsInline ref={firstPeerVideoRef}></video>
                     </div>
-                    <h2 className={styles['video-text']}>상대방 화면</h2>
+                    <h2 className={styles['video-text']}>{firstPeerVideoRef.userId ? firstPeerVideoRef.userId : ''}</h2>
                 </div>
                 {/* other screen */}
                 <div className={styles['screen']} style={!isOnlyMyVideoAvailable && isSecondPeerVideoReady ? { display: 'flex' } : { display: 'none' }}>
                     <div className={styles['video-box']}>
                         <video autoPlay playsInline ref={secondPeerVideoRef}></video>
                     </div>
-                    <h2 className={styles['video-text']}>상대방 화면</h2>
+                    <h2 className={styles['video-text']}>{secondPeerVideoRef.userId ? secondPeerVideoRef.userId : ''}</h2>
                 </div>
                 {/* other screen */}
                 <div className={styles['screen']} style={!isOnlyMyVideoAvailable && isThirdPeerVideoReady ? { display: 'flex' } : { display: 'none' }}>
                     <div className={styles['video-box']}>
                         <video autoPlay playsInline ref={thirdPeerVideoRef}></video>
                     </div>
-                    <h2 className={styles['video-text']}>상대방 화면</h2>
+                    <h2 className={styles['video-text']}>{thirdPeerVideoRef.userId ? thirdPeerVideoRef.userId : ''}</h2>
                 </div>
             </div>
             {
