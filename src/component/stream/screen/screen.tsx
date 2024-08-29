462c7bde5bd08ba5e567d41dc4e8580db0fb3645
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-29 18:21:20 +0900

    feat: replace svg

--- src/component/stream/screen/screen.tsx
+++ src/component/stream/screen/screen.tsx
@@ -51,11 +51,16 @@ const Screen = ({ isMyWebcamLoading, isOnlyMyVideoAvailable, setIsOnlyMyVideoAva
                         onSetEnlargedText(myAssignedId);
                         onSetEnlargedScreenRef(videoRef);
                     }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <path d="M21 21l-6-6m6 6v-4.8m0 4.8h-4.8" />
-                            <path d="M3 16.2V21m0 0h4.8M3 21l6-6" />
-                            <path d="M21 7.8V3m0 0h-4.8M21 3l-6 6" />
-                            <path d="M3 7.8V3m0 0h4.8M3 3l6 6" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 48 48" fill="none">
+                            <rect width="40" height="40" fill="white" fillOpacity="0.01" />
+                            <path d="M6 6L16 15.8995" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M6 41.8995L16 32" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M42.0001 41.8995L32.1006 32" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M41.8995 6L32 15.8995" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M33 6H42V15" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M42 33V42H33" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M15 42H6V33" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M6 15V6H15" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
                     </button>
                 </div>
@@ -70,11 +75,16 @@ const Screen = ({ isMyWebcamLoading, isOnlyMyVideoAvailable, setIsOnlyMyVideoAva
                         onSetEnlargedText(firstPeerVideoRef.userId);
                         onSetEnlargedScreenRef(firstPeerVideoRef);
                     }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <path d="M21 21l-6-6m6 6v-4.8m0 4.8h-4.8" />
-                            <path d="M3 16.2V21m0 0h4.8M3 21l6-6" />
-                            <path d="M21 7.8V3m0 0h-4.8M21 3l-6 6" />
-                            <path d="M3 7.8V3m0 0h4.8M3 3l6 6" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 48 48" fill="none">
+                            <rect width="40" height="40" fill="white" fillOpacity="0.01" />
+                            <path d="M6 6L16 15.8995" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M6 41.8995L16 32" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M42.0001 41.8995L32.1006 32" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M41.8995 6L32 15.8995" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M33 6H42V15" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M42 33V42H33" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M15 42H6V33" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M6 15V6H15" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
                     </button>
                 </div>
@@ -89,11 +99,16 @@ const Screen = ({ isMyWebcamLoading, isOnlyMyVideoAvailable, setIsOnlyMyVideoAva
                         onSetEnlargedText(secondPeerVideoRef.userId);
                         onSetEnlargedScreenRef(secondPeerVideoRef);
                     }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <path d="M21 21l-6-6m6 6v-4.8m0 4.8h-4.8" />
-                            <path d="M3 16.2V21m0 0h4.8M3 21l6-6" />
-                            <path d="M21 7.8V3m0 0h-4.8M21 3l-6 6" />
-                            <path d="M3 7.8V3m0 0h4.8M3 3l6 6" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 48 48" fill="none">
+                            <rect width="40" height="40" fill="white" fillOpacity="0.01" />
+                            <path d="M6 6L16 15.8995" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M6 41.8995L16 32" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M42.0001 41.8995L32.1006 32" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M41.8995 6L32 15.8995" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M33 6H42V15" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M42 33V42H33" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M15 42H6V33" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M6 15V6H15" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
                     </button>
                 </div>
@@ -108,11 +123,16 @@ const Screen = ({ isMyWebcamLoading, isOnlyMyVideoAvailable, setIsOnlyMyVideoAva
                         onSetEnlargedText(thirdPeerVideoRef.userId);
                         onSetEnlargedScreenRef(thirdPeerVideoRef);
                     }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <path d="M21 21l-6-6m6 6v-4.8m0 4.8h-4.8" />
-                            <path d="M3 16.2V21m0 0h4.8M3 21l6-6" />
-                            <path d="M21 7.8V3m0 0h-4.8M21 3l-6 6" />
-                            <path d="M3 7.8V3m0 0h4.8M3 3l6 6" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 48 48" fill="none">
+                            <rect width="40" height="40" fill="white" fillOpacity="0.01" />
+                            <path d="M6 6L16 15.8995" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M6 41.8995L16 32" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M42.0001 41.8995L32.1006 32" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M41.8995 6L32 15.8995" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M33 6H42V15" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M42 33V42H33" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M15 42H6V33" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M6 15V6H15" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
                     </button>
                 </div>
@@ -135,11 +155,16 @@ const Screen = ({ isMyWebcamLoading, isOnlyMyVideoAvailable, setIsOnlyMyVideoAva
                         <button className={styles['video-minimize-btn']} onClick={() => {
                             onCloseEnlargedScreen();
                         }}>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" width="40" height="40" viewBox="0 0 24 24">
-                                <path d="M9,3A1,1,0,0,0,8,4V6.59L3.71,2.29A1,1,0,0,0,2.29,3.71L6.59,8H4a1,1,0,0,0,0,2H8a2,2,0,0,0,2-2V4A1,1,0,0,0,9,3Z" />
-                                <path d="M16,10h4a1,1,0,0,0,0-2H17.41l4.3-4.29a1,1,0,1,0-1.42-1.42L16,6.59V4a1,1,0,0,0-2,0V8A2,2,0,0,0,16,10Z" />
-                                <path d="M8,14H4a1,1,0,0,0,0,2H6.59l-4.3,4.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L8,17.41V20a1,1,0,0,0,2,0V16A2,2,0,0,0,8,14Z" />
-                                <path d="M17.41,16H20a1,1,0,0,0,0-2H16a2,2,0,0,0-2,2v4a1,1,0,0,0,2,0V17.41l4.29,4.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z" />
+                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 48 48" fill="none">
+                                <rect width="40" height="40" fill="white" fillOpacity="0.01" />
+                                <path d="M6 6L16 15.8995" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M6 41.8995L16 32" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M42.0001 41.8995L32.1006 32" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M41.8995 6L32 15.8995" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M32 7V16H41" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M16 7V16H7" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M16 41V32H7" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M32 41V32H40.8995" stroke="#ffffff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
                             </svg>
                         </button>
                     </div>
