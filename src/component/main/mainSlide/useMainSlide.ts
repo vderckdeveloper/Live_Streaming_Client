c3199d8efb77105bca9ace6357b703cbf69e283d
vderckdeveloper <vderckdeveloper@gmail.com>
2024-08-30 13:28:08 +0900

    feat: slide image

--- src/component/main/mainSlide/useMainSlide.ts
+++ src/component/main/mainSlide/useMainSlide.ts
@@ -2,8 +2,12 @@ import { useState } from "react";
 import { useRouter } from 'next/navigation';
 import { SwiperClass } from "swiper/react";
 import { generateRoomCode } from "../../../../utils/utility";
-import groupVideoImage from '../../../../public/image/main/240730_mainSlide_Image(1)_Ver1.0.png';
-import recordVideoImage from '../../../../public/image/main/240730_mainSlide_Image(2)_Ver1.0.png';
+import SlideImageOne from '../../../../public/image/main/240730_mainSlide_Image(1)_Ver1.0.png';
+import SlideImageTwo from '../../../../public/image/main/240730_mainSlide_Image(2)_Ver1.0.png';
+import SlideImageThree from '../../../../public/image/main/240730_mainSlide_Image(3)_Ver1.0.png';
+import SlideImageFour from '../../../../public/image/main/240730_mainSlide_Image(4)_Ver1.0.png';
+import SlideImageFive from '../../../../public/image/main/240730_mainSlide_Image(5)_Ver1.0.png';
+import SlideImageSix from '../../../../public/image/main/240730_mainSlide_Image(6)_Ver1.0.png';
 import { StaticImageData } from "next/image";
 
 export interface SlideListProps {
@@ -35,12 +39,12 @@ export function useMainSlide() {
 
     // slide list
     const slideList: SlideListProps[] = [
-        { imagePath: groupVideoImage },
-        { imagePath: recordVideoImage },
-        { imagePath: groupVideoImage },
-        { imagePath: recordVideoImage },
-        { imagePath: groupVideoImage },
-        { imagePath: recordVideoImage },
+        { imagePath: SlideImageOne },
+        { imagePath: SlideImageTwo },
+        { imagePath: SlideImageThree },
+        { imagePath: SlideImageFour },
+        { imagePath: SlideImageFive },
+        { imagePath: SlideImageSix },
     ];
 
     return {
